dc0ea743667a9bb5b898cebaf9287ab2cc3abef4
Kat Marchán <kzm@zkat.tech>
2025-03-21 11:23:56 -0700

    fix(arborist): omit failed optional dependencies from installed deps, but keep them 'in the tree'
    Fixes: https://github.com/npm/cli/issues/4828
    Fixes: https://github.com/npm/cli/issues/7961
    Replaces: https://github.com/npm/cli/pull/8127
    Replaces: https://github.com/npm/cli/pull/8077
    
    When optional dependencies fail, we don't want to install them, for whatever reason. The way
    this "uninstallation" is done right now is by simply removing the dependencies from the ideal tree
    during reification.
    
    Unfortunately, this means that what gets saved to the "hidden lockfile" is the edited ideal tree
    as well, and when NPM runs next, it'll use that when regenerating the "real" package-lock.
    
    This PR tags failed optional deps such that they're still added to the "trash list" (and thus have
    their directories cleaned up by the reifier), but prevents Arborist from removing them altogether
    from the ideal tree (which is usually done by setting their parent to `null`, making them unreachable,
    and letting them get GC'd).

--- workspaces/arborist/lib/arborist/reify.js
+++ workspaces/arborist/lib/arborist/reify.js
@@ -150,7 +150,7 @@ module.exports = cls => class Reifier extends cls {
     for (const path of this[_trashList]) {
       const loc = relpath(this.idealTree.realpath, path)
       const node = this.idealTree.inventory.get(loc)
-      if (node && node.root === this.idealTree) {
+      if (node && node.root === this.idealTree && !node.ideallyInert) {
         node.parent = null
       }
     }
@@ -600,6 +600,7 @@ module.exports = cls => class Reifier extends cls {
     // retire the same path at the same time.
     const dirsChecked = new Set()
     return promiseAllRejectLate(leaves.map(async node => {
+      if (node.ideallyInert) { return }
       for (const d of walkUp(node.path)) {
         if (d === node.top.path) {
           break
@@ -744,6 +745,8 @@ module.exports = cls => class Reifier extends cls {
   }
 
   async #extractOrLink (node) {
+    if (node.ideallyInert) { return }
+
     const nm = resolve(node.parent.path, 'node_modules')
     await this.#validateNodeModules(nm)
 
@@ -819,6 +822,7 @@ module.exports = cls => class Reifier extends cls {
       const set = optionalSet(node)
       for (node of set) {
         log.verbose('reify', 'failed optional dependency', node.path)
+        node.ideallyInert = true
         this[_addNodeToTrashList](node)
       }
     }) : p).then(() => node)
@@ -1151,6 +1155,7 @@ module.exports = cls => class Reifier extends cls {
 
       this.#retiredUnchanged[retireFolder] = []
       return promiseAllRejectLate(diff.unchanged.map(node => {
+        if (node.ideallyInert) { return }
         // no need to roll back links, since we'll just delete them anyway
         if (node.isLink) {
           return mkdir(dirname(node.path), { recursive: true, force: true })
@@ -1230,7 +1235,7 @@ module.exports = cls => class Reifier extends cls {
       // skip links that only live within node_modules as they are most
       // likely managed by packages we installed, we only want to rebuild
       // unchanged links we directly manage
-      const linkedFromRoot = node.parent === tree || node.target.fsTop === tree
+      const linkedFromRoot = (node.parent === tree && !node.ideallyInert) || node.target.fsTop === tree
       if (node.isLink && linkedFromRoot) {
         nodes.push(node)
       }
